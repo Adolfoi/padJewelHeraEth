1c9acc839de3d5dd97b25d5f614effe59ba77440
Mitsuhiro Hashimoto <adolfoi.ios.developer@gmail.com>
2015-01-12 22:31:45 +0900

    added "265"

--- padJewelHeraEth.py
+++ padJewelHeraEth.py
@@ -16,7 +16,7 @@ def response(context, flow):
 			print "Length of the string: ", len(data['e']);
 			print "!!"
 			if dung.group(1) == '307' or dung.group(1) == '307' or dung.group(1) == '307' or dung.group(1) == '307' or dung.group(1) == '307': 
-				if len(data['e']) == 262 or len(data['e']) == 259 or len(data['e']) == 261 or len(data['e']) == 264 or len(data['e']) == 263 or len(data['e']) == 260 or len(data['e']) == 266: 
+				if len(data['e']) == 262 or len(data['e']) == 259 or len(data['e']) == 261 or len(data['e']) == 264 or len(data['e']) == 263 or len(data['e']) == 260 or len(data['e']) == 266 or len(data['e']) == 265: 
 					content = '{"res":99}' 
 					s = cStringIO.StringIO() 
 					gf = gzip.GzipFile(fileobj=s, mode='wb') 
